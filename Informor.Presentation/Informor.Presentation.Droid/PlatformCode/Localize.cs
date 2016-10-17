44f3b6ef9af376c0dacbfebb0acbbce7a77ec5d3
starlinerby <ignatstas94@gmail.com>
2016-10-17 21:51:49 +0300

    Localization added

---
+++ Informor.Presentation/Informor.Presentation.Droid/PlatformCode/Localize.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Threading;
+using Informor.Presentation.Droid.PlatformCode;
+using Informor.Presentation.Interfaces;
+using Java.Util;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Localize))]
+
+namespace Informor.Presentation.Droid.PlatformCode
+{
+    public class Localize : ILocalize
+    {
+        public void SetLocale(CultureInfo ci)
+        {
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            var androidLocale = Locale.Default;
+            var netLanguage = androidLocale.ToString().Replace("_", "-");
+            return CultureInfo.CurrentCulture;
+        }
+    }
+}
---
+++ Informor.Presentation/Informor.Presentation.iOS/PlatformCode/Localize.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System.Threading;
+using Foundation;
+using Informor.Presentation.iOS.PlatformCode;
+using Informor.Presentation.Interfaces;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Localize))]
+namespace Informor.Presentation.iOS.PlatformCode
+{
+    class Localize : ILocalize
+    {
+        public void SetLocale(CultureInfo ci)
+        {
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+        private const string DefaultCulture = "en";
+
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            CultureInfo result = new CultureInfo(DefaultCulture);
+            try
+            {
+                string netLanguage = DefaultCulture;
+                if (NSLocale.PreferredLanguages.Length > 0)
+                {
+                    var pref = NSLocale.PreferredLanguages[0];
+                    netLanguage = pref.Replace("_", "-");
+
+                    result = new CultureInfo(netLanguage);
+                }
+            }
+            catch (CultureNotFoundException ex)
+            {
+                result = new CultureInfo(DefaultCulture);
+            }
+
+            return result;
+        }
+    }
+}
